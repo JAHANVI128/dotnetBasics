52120959173e427a352f083a29c09c4a0d2c750b
acer <acer@PRO500AM4-M5>
2024-03-13 18:01:28 +0530

    GET, POST, Query Helpers

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Primitives;
+using System.IO;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -26,7 +29,11 @@ app.Run(async (HttpContext context) =>
     await context.Response.WriteAsync($"Method: {method}");*/
 
     // Query String
-    context.Response.Headers["Content-type"] = "text/html";
+    //context.Response.Headers["Content-type"] = "text/html";
+    //if (context.Request.Headers.ContainsKey("User-Agent")) {
+    //    string userAgent = context.Request.Headers["User-Agent"];
+    //    await context.Response.WriteAsync($"<p>User-Agent: {userAgent}</p>");
+    //} 
 
     /*if(context.Request.Method == "POST") {
             string name = context.Request.Form["name"];
@@ -36,21 +43,34 @@ app.Run(async (HttpContext context) =>
         } else {
             await context.Response.WriteAsync("Hello World!");
         }*/
-    if(context.Request.Method == "GET") {
-        //string name = context.Request.Query["name"];
-        //string email = context.Request.Query["email"];
-        //await context.Response.WriteAsync($"Name: {name}");
-        //await context.Response.WriteAsync($"Email: {email}");
-        if(context.Request.Query.ContainsKey("id")) {
-            string id = context.Request.Query["id"];
-            await context.Response.WriteAsync($"ID : {id}");
-        } else {
-            await context.Response.WriteAsync("ID not found");
-        }
-    }
-    else {
-        await context.Response.WriteAsync("Hello World!");
-    }
+    //if(context.Request.Method == "GET") {
+    //string name = context.Request.Query["name"];
+    //string email = context.Request.Query["email"];
+    //await context.Response.WriteAsync($"Name: {name}");
+    //await context.Response.WriteAsync($"Email: {email}");
+    //    if(context.Request.Query.ContainsKey("id")) {
+    //        string id = context.Request.Query["id"];
+    //        await context.Response.WriteAsync($"ID : {id}");
+    //    } else {
+    //        await context.Response.WriteAsync("ID not found");
+    //    }
+    //}
+    //else {
+    //    await context.Response.WriteAsync("Hello World!");
+    //}
+
+    //GET POST
+    StreamReader reader = new StreamReader(context.Request.Body);
+    string body = await reader.ReadToEndAsync();
+    Dictionary<string, StringValues> queryDict =  Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
+
+    //if (body == "Jahanvi here")
+    //{
+    //    context.Response.StatusCode = 200;
+    //} else
+    //{
+    //    context.Response.StatusCode = 500;
+    //}
 });
 
 app.Run();
